04db9f4b5b9302a13615e8fba53416dafcdef763
hemangi-weybee <hemangi@webtheuniverse.com>
2022-11-12 16:03:58 +0530

    removed

--- react-starwarss/src/reduxHelper/store.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from 'redux-thunk';
-import fetchReducer from "./fetchReducer";
-import { composeWithDevTools } from 'redux-devtools-extension';
-
-export const store = createStore(fetchReducer, composeWithDevTools(applyMiddleware(thunk)))
-
-export default store
